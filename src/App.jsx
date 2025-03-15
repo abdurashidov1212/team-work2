553d3a0e3958e14e0a60ddb27c2165f62ce314da
xojakbar <abdurashidovxojiakbar098@gmail.com>
2025-03-15 15:16:40 +0500

    xojiakbar qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css'
 import About from './pages/about'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <h1>Hello main</h1>
     <h2>Hello jsx</h2>
+    <h3>Xojiakbar qoshildi</h3>
     <About/>
     </>
   )
